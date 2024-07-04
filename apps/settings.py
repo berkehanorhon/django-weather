1bb48f58080ef2cd7d747872fe93e25a0c2dd7f2
Berkehan <b2200356072@cs.hacettepe.edu.tr>
2024-07-04 17:31:30 +0300

    updates

--- apps/settings.py
+++ apps/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'weather.apps.WeatherConfig',
-    'weather'
+    'weather',
+    'users',
 ]
 
 MIDDLEWARE = [
