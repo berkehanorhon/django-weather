462666dfea69a3468465609dd5fbc9d46f23302d
Berkehan <b2200356072@cs.hacettepe.edu.tr>
2024-07-05 18:02:29 +0300

    new api and views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from .forms import RegisterForm, LoginForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                #messages.success(request, 'Başarıyla giriş yaptınız.')
+                return redirect('home')
+            else:
+                pass
+                #messages.error(request, 'Geçersiz kullanıcı adı veya şifre.')
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            #messages.success(request, 'Başarıyla kayıt oldunuz.')
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    #messages.error(request, 'Kayıt işlemi sırasında hata! Lütfen tekrar deneyiniz.')
+    return render(request, 'users/register.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
--- weather/views.py
+++ weather/views.py
@@ -1,17 +1,24 @@
 import requests
 from django.conf import settings
 from rest_framework.decorators import api_view
+from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Location
+from .models import Location, QueryLog
 from .serializers import LocationInfoSerializer
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import time
 
+@login_required
 @api_view(['GET'])
 def get_location_info(request, location_id):
     """
     Get given location's weather information.
     """
+    start = time.time()
+    QueryLogEntity = QueryLog.objects.create(user=request.user, location_id=location_id,
+                                       user_ip=request.META.get('REMOTE_ADDR'))
     try:
         location = Location.objects.get(id=location_id)
         location_name = location.name
@@ -27,6 +34,7 @@ def get_location_info(request, location_id):
         serializer = LocationInfoSerializer(data=data)
 
         if serializer.is_valid():
+            QueryLogEntity.status = 'SUCCESS'
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -35,9 +43,28 @@ def get_location_info(request, location_id):
         return Response({'error': 'Location not found'}, status=status.HTTP_404_NOT_FOUND)
     except requests.RequestException as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    finally:
+        QueryLogEntity.query_duration = time.time() - start
+        QueryLogEntity.query_result = 'DONE'
+        QueryLogEntity.save()
 
-def get_weather_info(request, location_id):
-    context = {
-        'location_id': location_id
-    }
-    return render(request, 'weather/weather_info.html', context=context)
+@api_view(['GET'])
+@login_required
+def get_all_locations(request):
+    locations = Location.objects.all().values('id', 'name')
+    locations_list = list(locations)
+    return JsonResponse(locations_list, safe=False)
+
+@api_view(['GET'])
+@login_required
+def get_my_logs_api(request):
+    logs = QueryLog.objects.filter(user=request.user).values('location__name', 'query_time', 'user_ip', 'query_result', 'query_duration', 'status').order_by('-query_time')
+    logs_list = list(logs)
+    return JsonResponse(logs_list, safe=False)
+
+@login_required
+def get_weather_info(request):
+    return render(request, 'weather/weather_info.html')
+
+def get_my_logs(request):
+    return render(request, 'weather/my_logs.html')
