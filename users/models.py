cb0ff753b7034d7995cd4286842f917897eb9723
Berkehan <b2200356072@cs.hacettepe.edu.tr>
2024-07-05 14:44:26 +0300

    model update

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 
+
 # Create your models here.
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -18,10 +19,13 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_admin', True)
         extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
         if extra_fields.get('is_admin') is not True:
             raise ValueError('Superuser must have is_admin=True.')
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_admin=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
         return self.create_user(email, password, **extra_fields)
 
 
--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=200, unique=True, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class QueryLog(models.Model):
+    STATUS_CHOICES = (
+        ('SUCCESS', 'Başarılı'),
+        ('FAILURE', 'Başarısız'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='query_logs')
+    query_time = models.DateTimeField(default=timezone.now)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='query_logs')
+    user_ip = models.GenericIPAddressField()
+    query_result = models.TextField()
+    query_duration = models.IntegerField()
+    status = models.CharField(max_length=7, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.user} - {self.location.name} - {self.query_time} - {self.status}"
